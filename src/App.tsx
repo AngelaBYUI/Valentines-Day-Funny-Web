2cb83e175d0b3a8fc3a1e1b699944dc8ce222e18
Angela Chen <che25017@byui.edu>
2026-02-05 18:35:24 -0700

    my change

--- src/App.tsx
+++ src/App.tsx
@@ -9,46 +9,49 @@ export default function Page() {
   const handleNoClick = () => {
     setNoCount(noCount + 1);
   };
-
+const firstPage = (
+  <>
+    <img
+      className="h-[200px]"
+      style={{ width: "400px", height: "240px" }}
+      src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+    />
+    <h1 className="text-container">Will you be my Valentine?</h1>
+  </>
+);
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "真的嗎?",
+      // "Really sure?",
+      // "Think again!",
+      // "Last chance!",
+      // "Surely not?",
+      // "You might regret this!",
+      // "Give it another thought!",
+      // "Are you absolutely certain?",
+      // "This could be a mistake!",
+      // "Have a heart!",
+      "你捨得嗎?",
+      // "Change of heart?",
+      "確定不再想想?",
+      "這是最後的機會囉?"
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
-
+  // const noText = getNoButtonText();
   return (
     <div className="centered-container">      
       <div className="valentine-container">
         {yesPressed ? (
           <>
-            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div className="text-container">Ok yay!!!</div>
+            <img src="https://media1.tenor.com/m/seQ7gcIAzPMAAAAd/memes.gif" />
+            <div className="text-container">抓到!我就知道你偷偷暗戀我!!!😜</div>
           </>
         ) : (
           <>
-            <img
-              className="h-[200px]"
-              style={{ width: "400x", height: "240px" }}
-              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-            />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            {noCount < 5 && firstPage}
             <div>
               <button
                 className={"yes-button"}
@@ -58,9 +61,22 @@ export default function Page() {
                 Yes
               </button>
 
-              <button onClick={handleNoClick} className="no-button">
-                {noCount === 0 ? "No" : getNoButtonText()}
-              </button>
+              {/* <button onClick={handleNoClick} className="no-button"> */}
+                {/* {noCount === 0 ? "No" : noText} */}
+              {/* </button> */}
+              { noCount >4 &&(
+                <img
+                  src="https://media1.tenor.com/m/dhuQV_msfiUAAAAd/cat-gun.gif"
+                  alt="Final warning"
+                  className="final-image"
+                />
+              )}
+              {noCount < 5 && (
+                <button onClick={handleNoClick} className="no-button">
+                  {noCount === 0 ? "No" : getNoButtonText()}
+                </button>
+              )}
+
             </div>
           </>
         )}
