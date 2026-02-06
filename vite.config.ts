6f715c80fc41acef76953edc7b87425fe3660ebc
Angela Chen <che25017@byui.edu>
2026-02-05 18:45:21 -0700

    didn't have the slash

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'Valentines-Day-Funny-Web'
+  base:'/Valentines-Day-Funny-Web/'
 });
